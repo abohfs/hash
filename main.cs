beede70362a840492f762e7608d22279ad5752d0
ahmed frhat <email.ahmedfrhat@gmial.com>
2025-07-23 14:11:21 +0300

    adding the user info again.

--- main.cs
+++ main.cs
@@ -1,6 +1,16 @@
 
 // to change set a user passward.
 int continueOrNot=0;
-do{
-  
-}while(continueOrNot!=0)
+do
+{
+    // taking user info.
+    Console.Write("Enter your username : ");
+    string userName = Console.ReadLine();
+
+    Console.Write("\nEnter your passward : ");
+    string userPassward = Console.ReadLine();
+
+    // printing operation message.
+    Console.Write($"\nHello {userName}, you Passward saved succesfully.");
+
+} while (continueOrNot != 0);
