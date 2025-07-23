b5cd41d3a771022115c519db589d2751c7e01ecd
abohfs <email.ahmedfrhat@gmail.com>
2025-07-23 13:49:12 +0300

    Create main c sharp file.

---
+++ main.cs
@@ -0,0 +1,10 @@
+
+// taking user info.
+Console.Write("Enter your username : ");
+string userName = Console.ReadLine();
+
+Console.Write("\nEnter your passward : ");
+string userPassward = Console.ReadLine();
+
+// printing operation message.
+Console.Write($"\nHello {userName}, you Passward saved succesfully.");
